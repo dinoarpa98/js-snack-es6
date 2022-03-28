e3fcd453b6932db96dff5d03db4eaa193361ad39
dinoarpa98 <dinoarpa98@gmail.com>
2022-03-28 16:31:47 +0200

    end

--- js-4/asset/main.js
+++ js-4/asset/main.js
@@ -12,3 +12,81 @@
 // 1- Dobbiamo creare delle targhe con il loro nome in maiuscolo. Ci serve quindi un nuovo array di stringhe. Ogni elemento del nuovo array sarà il nome dello studente ma con tutte le lettere maiuscole.
 // 2-  Dobbiamo creare un nuovo array con gli studenti che hanno un totale di voti superiore a 70
 // 3-  Dobbiamo creare un nuovo array di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+
+
+
+// let text = "Hello World!";
+// let result = text.toUpperCase();
+
+
+
+let students = [{
+        name: 'Marco',
+        id: 213,
+        grades: 78
+    },
+    {
+        name: 'Paola',
+        id: 110,
+        grades: 96
+    },
+    {
+        name: 'Andrea',
+        id: 250,
+        grades: 48
+    },
+    {
+        name: 'Gaia',
+        id: 145,
+        grades: 74
+    },
+    {
+        name: 'Luigi',
+        id: 196,
+        grades: 68
+    },
+    {
+        name: 'Piero',
+        id: 102,
+        grades: 50
+    },
+    {
+        name: 'Francesca',
+        id: 120,
+        grades: 84
+    },
+];
+
+
+const targheStudenti = students.map(
+    (element) => {
+
+        return element.name.toUpperCase();
+    }
+)
+
+console.log(targheStudenti);
+
+
+
+const min70 = students.filter(
+    (element) => {
+        if (element.grades > 70) {
+            return true;
+        }
+    }
+)
+
+console.log(min70);
+
+
+
+const min70id120 = students.filter(
+    (element) => {
+        if (element.grades > 70 && element.id > 120) {
+            return true;
+        }
+    }
+)
+
+console.log(min70id120);
